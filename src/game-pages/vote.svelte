350adaed085c27962508eef37b1d007c2a031eec
Noah Geren <40411652+noahgeren@users.noreply.github.com>
2022-05-29 11:57:30 -0500

    Added pop animation when card is selected

--- src/game-pages/vote.svelte
+++ src/game-pages/vote.svelte
@@ -9,20 +9,21 @@
     <h1 class="text-4xl mx-3 mb-4 font-semibold">I went to the desert and ate of the peyote cactus. Turns out my spirit animal is _____.</h1>
     {#each whiteCards as card, idx}
     <div
-        class="card bg-gray-100 border border-gray-400 text-black shadow shadow-black"
+        class="card bg-gray-100 border border-gray-400 text-black shadow shadow-black btn-pop cursor-pointer"
         class:h-card={idx === whiteCards.length - 1}
         class:-mt-8={idx !== 0}
         class:bg-primary={selected === idx}
         class:text-white={selected === idx}
-        class:cursor-pointer={selected !== idx}
         on:click={() => selected = idx}
     >
-        <div class="card-body pt-2" class:mb-4={idx !== whiteCards.length - 1}>
+        <div class="card-body pt-3" class:mb-4={idx !== whiteCards.length - 1}>
             <div class="card-title text-4xl">{@html card}</div>
         </div>
     </div> 
     {/each}
 </div>
+{#if selected !== null}
 <div class="fixed bottom-4 w-full flex justify-center">
     <button class="btn btn-lg btn-primary shadow-xl outline outline-2 text-white">Submit</button>
 </div>
+{/if}

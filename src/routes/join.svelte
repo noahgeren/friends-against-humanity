17ba03eba4e1ca341812164085ae2b6524f66bb3
Noah Geren <40411652+noahgeren@users.noreply.github.com>
2022-05-29 12:38:21 -0500

    Fixed some problems on mobile

--- src/routes/join.svelte
+++ src/routes/join.svelte
@@ -1,10 +1,15 @@
-<div class="card w-full max-w-sm h-card bg-gray-100 text-black mx-auto">
+<div class="card w-full max-w-sm h-card bg-gray-100 text-black mx-auto mt-3">
     <div class="card-body">
         <a href="/" class="card-title text-6xl">Friends<br/>Against<br/>Humanity</a>
-        <input placeholder="Access Code" class="input input-lg input-bordered input-primary text-white w-full mt-3" />
-        <input placeholder="Nickname" class="input input-lg input-bordered input-primary text-white w-full" />
+        <input placeholder="Access Code" class="input input-lg text-white w-full mt-3" />
+        <input placeholder="Nickname" class="input input-lg text-white w-full" />
         <div class="flex justify-center mt-3">
             <button class="btn btn-lg btn-primary text-white shadow">Join Game</button>
         </div>
     </div>
 </div>
+<style>
+    .input {
+        outline: 0 !important;
+    }
+</style>
